d52445acb416bd95181895dd1df9886e385f40b6
joviksdev <to.emmanuel93@gmail.com>
2025-01-30 13:44:49 +0100

    Fix build issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-// import {terser} from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
-const {terser} = require('rollup-plugin-terser');
+// const {terser} = require('rollup-plugin-terser');
 // const typescript = require('@rollup/plugin-typescript');
 
 export default {
